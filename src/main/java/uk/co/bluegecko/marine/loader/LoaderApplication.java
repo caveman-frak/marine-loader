1e316c9afd7ed7e255975d8038eebe771a59d28f
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-05-23 19:02:05 +0100

    incremental vessel model validation handling

--- src/main/java/uk/co/bluegecko/marine/loader/LoaderApplication.java
+++ src/main/java/uk/co/bluegecko/marine/loader/LoaderApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import uk.co.bluegecko.marine.shared.application.AbstractApplication;
 
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
@@ -14,12 +14,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 		HibernateJpaAutoConfiguration.class
 })
 @ConfigurationPropertiesScan
-public class LoaderApplication {
+public class LoaderApplication extends AbstractApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(LoaderApplication.class)
-				.registerShutdownHook(true)
-				.web(WebApplicationType.NONE)
-				.run(args);
+		exit(run(LoaderApplication.class, WebApplicationType.NONE, args));
 	}
 }
