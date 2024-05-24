79e9da840e133f0f5b779e256b14c17df9d685c5
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-05-24 18:19:46 +0100

    recovery post deletion

--- src/test/java/uk/co/bluegecko/marine/loader/common/files/AbstractExtractorTest.java
+++ src/test/java/uk/co/bluegecko/marine/loader/common/files/AbstractExtractorTest.java
@@ -2,11 +2,8 @@ package uk.co.bluegecko.marine.loader.common.files;
 
 import static java.lang.ClassLoader.getSystemResource;
 import static java.lang.ClassLoader.getSystemResourceAsStream;
-import static org.assertj.core.api.Assertions.assertThat;
-import static uk.co.bluegecko.marine.test.jassert.Conditions.extracted;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
@@ -21,77 +18,14 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import lombok.NonNull;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import uk.co.bluegecko.marine.wire.batch.Batch;
 import uk.co.bluegecko.marine.wire.batch.BatchType;
 import uk.co.bluegecko.marine.wire.batch.Batchable;
 
 public class AbstractExtractorTest {
-
-	@Test
-	@Tag("sanity-check")
-	@DisplayName("Sanity check the CSV file")
-	void checkCsvFileContents() throws IOException {
-		try (BufferedReader reader = bufferedReader(data(), csv())) {
-			assertThat(reader.readLine())
-					.startsWith("\"id\",")
-					.endsWith(",\"country\"");
-		}
-	}
-
-	@Test
-	@Tag("sanity-check")
-	@DisplayName("Sanity check the JSON file")
-	void checkJsonFileContents() throws IOException {
-		try (BufferedReader reader = bufferedReader(data(), json())) {
-			assertThat(reader.readLine())
-					.isEqualTo("{");
-		}
-	}
-
-	@Test
-	@Tag("sanity-check")
-	@DisplayName("Sanity check the ZIP file")
-	void checkRawZipFileContents() throws IOException {
-		try (InputStream in = inputStream(data(), zip())) {
-			assertThat(in.readNBytes(4)).as("Header 'PK♥♦'")
-					.isEqualTo(new byte[]{80, 75, 3, 4});
-			assertThat(in.readNBytes(2)).as("Version .0")
-					.isEqualTo(new byte[]{20, 0});
-			assertThat(in.readNBytes(2)).as("Bit flag 2 0")
-					.isEqualTo(new byte[]{2, 0});
-			assertThat(in.readNBytes(2)).as("Compression 8 0")
-					.isEqualTo(new byte[]{8, 0});
-		}
-	}
-
-	@Test
-	@Tag("sanity-check")
-	@DisplayName("Sanity check the ZIP file contents")
-	void checkZipFileContents() throws IOException {
-		try (ZipInputStream zin = zipInputStream(data())) {
-			// CSV file
-			assertThat(zin.getNextEntry())
-					.has(extracted(ZipEntry::getName, "file name of", "dummy-data.csv"));
-			BufferedReader csvReader = new BufferedReader(new InputStreamReader(zin));
-			assertThat(csvReader.lines().toList())
-					.hasSize(4);
-			zin.closeEntry();
-			// JSON file
-			assertThat(zin.getNextEntry())
-					.has(extracted(ZipEntry::getName, "file name of", "dummy-data.json"));
-			BufferedReader jsonReader = new BufferedReader(new InputStreamReader(zin));
-			assertThat(jsonReader.lines().toList())
-					.hasSize(200);
-			zin.closeEntry();
-		}
-	}
 
 	protected InputStream inputStream(@NonNull final String dir, @NonNull final String suffix) {
 		return Objects.requireNonNull(getSystemResourceAsStream(dir + "/" + file() + "." + suffix));
@@ -199,7 +133,7 @@ public class AbstractExtractorTest {
 		@Override
 		public Batch collect(@NonNull final Path file,
 				@NonNull final Map<Enum<?>, List<ParseResult>> results) {
-			ParseResult result = results.get(DummyType.CSV).get(0);
+			ParseResult result = results.get(DummyType.CSV).getFirst();
 			return Batch.builder()
 					.type(BatchType.MIXED)
 					.file(file.resolve(result.file()))
---
+++ src/test/java/uk/co/bluegecko/marine/loader/common/files/SanityCheckTest.java
@@ -0,0 +1,77 @@
+package uk.co.bluegecko.marine.loader.common.files;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.co.bluegecko.marine.test.jassert.Conditions.extracted;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SanityCheckTest extends AbstractExtractorTest {
+
+	@Test
+	@Tag("sanity-check")
+	@DisplayName("Sanity check the CSV file")
+	void checkCsvFileContents() throws IOException {
+		try (BufferedReader reader = bufferedReader(data(), csv())) {
+			assertThat(reader.readLine())
+					.startsWith("\"id\",")
+					.endsWith(",\"country\"");
+		}
+	}
+
+	@Test
+	@Tag("sanity-check")
+	@DisplayName("Sanity check the JSON file")
+	void checkJsonFileContents() throws IOException {
+		try (BufferedReader reader = bufferedReader(data(), json())) {
+			assertThat(reader.readLine())
+					.isEqualTo("{");
+		}
+	}
+
+	@Test
+	@Tag("sanity-check")
+	@DisplayName("Sanity check the ZIP file")
+	void checkRawZipFileContents() throws IOException {
+		try (InputStream in = inputStream(data(), zip())) {
+			assertThat(in.readNBytes(4)).as("Header 'PK♥♦'")
+					.isEqualTo(new byte[]{80, 75, 3, 4});
+			assertThat(in.readNBytes(2)).as("Version .0")
+					.isEqualTo(new byte[]{20, 0});
+			assertThat(in.readNBytes(2)).as("Bit flag 2 0")
+					.isEqualTo(new byte[]{2, 0});
+			assertThat(in.readNBytes(2)).as("Compression 8 0")
+					.isEqualTo(new byte[]{8, 0});
+		}
+	}
+
+	@Test
+	@Tag("sanity-check")
+	@DisplayName("Sanity check the ZIP file contents")
+	void checkZipFileContents() throws IOException {
+		try (ZipInputStream zin = zipInputStream(data())) {
+			// CSV file
+			assertThat(zin.getNextEntry())
+					.has(extracted(ZipEntry::getName, "file name of", "dummy-data.csv"));
+			BufferedReader csvReader = new BufferedReader(new InputStreamReader(zin));
+			assertThat(csvReader.lines().toList())
+					.hasSize(4);
+			zin.closeEntry();
+			// JSON file
+			assertThat(zin.getNextEntry())
+					.has(extracted(ZipEntry::getName, "file name of", "dummy-data.json"));
+			BufferedReader jsonReader = new BufferedReader(new InputStreamReader(zin));
+			assertThat(jsonReader.lines().toList())
+					.hasSize(200);
+			zin.closeEntry();
+		}
+	}
+	
+}
