223eae5ef336772aac2f4043a64e1022c3136f30
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-09-29 18:03:05 +0100

    upgrade java, gradle and library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("com.opencsv:opencsv:5.9") {
         exclude("commons-collections", "commons-collections")
     }
-    implementation("org.apache.commons:commons-collections4:4.5.0-M1")
+    implementation("org.apache.commons:commons-collections4:4.5.0-M2")
     implementation("dk.dma.enav:enav-serialization:0.6")
     implementation("dk.dma.ais.lib:ais-lib-utils:2.8.4") {
         exclude("com.beust", "jcommander")
